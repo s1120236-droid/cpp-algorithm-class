66a5f0ecbea7aa5d452f4339a71ba29fa76603c6
s1120236-droid <s1120236@dmhs.kh.edu.tw>
2025-10-16 14:47:45 +0800

    hw5

--- main.cpp
+++ main.cpp
@@ -2,11 +2,26 @@
 using namespace std;
 
 int main() {
-    int sum = 0;  // (This variable is unused)
+    // 使用for迴圈列印星號三角形
+    cout << "Star Triangle Pattern (using for loop):\n";
+    for (int i = 1; i <= 6; ++i) {  // 外層迴圈，控制列數
+        for (int j = 1; j <= i; ++j) {  // 內層迴圈，控制每列的星號數量
+            cout << "*";  // 印出一顆星號
+        }
+        cout << endl;  // 換行
+    }
 
-    for (int i = 1; i <= 50; i++) {
-        if ((i % 7) == 0)
-            cout << i << " ";
+    cout << "\nMultiplication Table (using while loop):\n";
+    // 使用while迴圈列印九九乘法表
+    int i = 1;
+    while (i <= 9) {  // 外層迴圈控制行數
+        int j = 1;
+        while (j <= 9) {  // 內層迴圈控制列數
+            cout << i << " * " << j << " = " << i * j << "\t";  // 印出乘法表的一行
+            j++;  // j自增
+        }
+        cout << endl;  // 換行
+        i++;  // i自增
     }
 
     return 0;
